691a4c70b727d77c63d9594a290e1c0647150189
Nilesh <nilesh507@gmail.com>
2020-07-30 21:42:01 +0530

    Managing states of cartItems

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,48 +1,69 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-    constructor () {
-        super();
-        this.state = {
-            price: 999,
-            title: 'Mobile Phone',
-            Qty: 1,
-            img: ''
-        }
-        //Can bind multiple event handlers can be bind over here 
-        // this.increaseQuantity = this.increaseQuantity.bind(this);
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: 'Mobile Phone',
+      Qty: 1,
+      img: ''
     }
-    increaseQuantity = () => {
-        console.log('this.state', this.state);
-    }
-  render () {
-    const {title, price, Qty} = this.state;
+    //Can bind multiple event handlers can be bind over here 
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+  }
+  increaseQuantity = () => {
+    // this.state.Qty += 1;
+    console.log('this.state', this.state);
+    // setState form 1 
+    // this.setState({
+    //   Qty: this.state.Qty + 1 
+    // });
+
+    //setState form 2 - if previous state is required 
+    this.setState((prevState) => {
+      return {
+        Qty: prevState.Qty + 1
+      }
+    });
+  }
+  decreaseQuantitiy= () => {
+    console.log('this.state', this.state);
+    this.setState((prevState) => {
+      return {
+        Qty: prevState.Qty - 1
+      }
+    });
+  }
+  render() {
+    const { title, price, Qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
-          <img style={styles.image}  />
+          <img style={styles.image} />
         </div>
         <div className="right-block">
-          <div style={ { fontSize: 25 } }>{title}</div>
-          <div style={ { color: '#777' } }>{price}</div>
-          <div style={ { color: '#777' } }>{Qty}</div>
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: '#777' }}>{price}</div>
+          <div style={{ color: '#777' }}>{Qty}</div>
           <div className="cart-item-actions">
             {/* Buttons */}
-            <img 
-                alt="increase" 
-                className="action-icons" 
-                src="https://image.flaticon.com/icons/png/512/3114/3114793.png"
-                onClick={this.increaseQuantity} 
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/png/512/3114/3114793.png"
+              onClick={this.increaseQuantity}
             />
-            <img 
-                alt="decrease" 
-                className="action-icons" 
-                src="https://www.flaticon.com/premium-icon/icons/svg/3114/3114894.svg" 
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://www.flaticon.com/premium-icon/icons/svg/3114/3114894.svg" 
+              onClick={this.decreaseQuantitiy}
             />
-            <img 
-                alt="delete" 
-                className="action-icons" 
-                src="https://image.flaticon.com/icons/svg/1214/1214428.svg" 
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
             />
 
           </div>
