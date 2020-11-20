a8cf28a7d57edc5b4acafe3c872d81da35f1acd4
Nilesh <nilesh507@gmail.com>
2020-11-20 17:49:03 +0530

    setting up firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import  firebase from 'firebase';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgAuN02xCuOJx-W40vGl86xFSaefqx6hQ",
+  authDomain: "cart-702ea.firebaseapp.com",
+  databaseURL: "https://cart-702ea.firebaseio.com",
+  projectId: "cart-702ea",
+  storageBucket: "cart-702ea.appspot.com",
+  messagingSenderId: "679582416662",
+  appId: "1:679582416662:web:bf612e2c49dbd0e372da37"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
